9ef54a305c4c99ef8407b30df1f6d3f076773781
Tomas Garcia <tomasotano25@gmail.com>
2022-10-18 12:35:27 -0400

    update docs

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from routes.user import user
 
 app = FastAPI(
     title="CRUD API with FastAPI",
+    description="Esta es una Api con FastAPI",
+    version="0.0.1",
     openapi_tags=[
         {"name": "users", "description": "users routes"}
     ]
